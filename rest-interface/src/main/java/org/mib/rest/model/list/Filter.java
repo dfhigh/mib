0f5e9a7b850d6fecdeae814807f3ef2cba7e6273
Fei Du <fei.du@autodesk.com>
2019-02-14 18:28:44 +0800

    add static builders for sorter and filter

--- rest-interface/src/main/java/org/mib/rest/model/list/Filter.java
+++ rest-interface/src/main/java/org/mib/rest/model/list/Filter.java
@@ -1,16 +1,39 @@
 package org.mib.rest.model.list;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Filter {
+
     private String field;
     private Operator operator;
     private Object value;
+
+    public static Filter eq(final String field, final Object value) {
+        return new Filter(field, Operator.EQ, value);
+    }
+
+    public static Filter ne(final String field, final Object value) {
+        return new Filter(field, Operator.NE, value);
+    }
+
+    public static Filter lt(final String field, final Object value) {
+        return new Filter(field, Operator.LT, value);
+    }
+
+    public static Filter lte(final String field, final Object value) {
+        return new Filter(field, Operator.LTE, value);
+    }
+
+    public static Filter gt(final String field, final Object value) {
+        return new Filter(field, Operator.GT, value);
+    }
+
+    public static Filter gte(final String field, final Object value) {
+        return new Filter(field, Operator.GTE, value);
+    }
 }
--- rest-interface/src/main/java/org/mib/rest/model/list/Sorter.java
+++ rest-interface/src/main/java/org/mib/rest/model/list/Sorter.java
@@ -1,15 +1,21 @@
 package org.mib.rest.model.list;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sorter {
     private String field;
     private Order order;
+
+    public static Sorter ascending(final String field) {
+        return new Sorter(field, Order.ASC);
+    }
+
+    public static Sorter descending(final String field) {
+        return new Sorter(field, Order.DESC);
+    }
 }
