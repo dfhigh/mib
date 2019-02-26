c77c757b796d0ab6d6fd6710d822033acb220f33
Fei Du <fei.du@autodesk.com>
2019-02-26 14:56:08 +0800

    support json property in Filter and Sorter

--- rest-interface/src/main/java/org/mib/rest/model/list/Filter.java
+++ rest-interface/src/main/java/org/mib/rest/model/list/Filter.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.mib.common.validator.Validator.validateStringNotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -12,28 +14,59 @@ public class Filter {
     private String field;
     private Operator operator;
     private Object value;
+    private String jsonKey;
 
     public static Filter eq(final String field, final Object value) {
-        return new Filter(field, Operator.EQ, value);
+        return eq(field, value, null);
+    }
+
+    public static Filter eq(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.EQ, value, jsonKey);
     }
 
     public static Filter ne(final String field, final Object value) {
-        return new Filter(field, Operator.NE, value);
+        return ne(field, value, null);
+    }
+
+    public static Filter ne(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.NE, value, jsonKey);
     }
 
     public static Filter lt(final String field, final Object value) {
-        return new Filter(field, Operator.LT, value);
+        return lt(field, value, null);
+    }
+
+    public static Filter lt(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.LT, value, jsonKey);
     }
 
     public static Filter lte(final String field, final Object value) {
-        return new Filter(field, Operator.LTE, value);
+        return lte(field, value, null);
+    }
+
+    public static Filter lte(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.LTE, value, jsonKey);
     }
 
     public static Filter gt(final String field, final Object value) {
-        return new Filter(field, Operator.GT, value);
+        return gt(field, value, null);
+    }
+
+    public static Filter gt(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.GT, value, jsonKey);
     }
 
     public static Filter gte(final String field, final Object value) {
-        return new Filter(field, Operator.GTE, value);
+        return gte(field, value, null);
+    }
+
+    public static Filter gte(final String field, final Object value, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Filter(field, Operator.GTE, value, jsonKey);
     }
 }
--- rest-interface/src/main/java/org/mib/rest/model/list/Sorter.java
+++ rest-interface/src/main/java/org/mib/rest/model/list/Sorter.java
@@ -4,18 +4,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.mib.common.validator.Validator.validateStringNotBlank;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sorter {
+
     private String field;
     private Order order;
+    private String jsonKey;
 
     public static Sorter ascending(final String field) {
-        return new Sorter(field, Order.ASC);
+        return ascending(field, null);
+    }
+
+    public static Sorter ascending(final String field, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Sorter(field, Order.ASC, jsonKey);
     }
 
     public static Sorter descending(final String field) {
-        return new Sorter(field, Order.DESC);
+        return descending(field, null);
+    }
+
+    public static Sorter descending(final String field, final String jsonKey) {
+        validateStringNotBlank(field, "field name");
+        return new Sorter(field, Order.DESC, jsonKey);
     }
 }
