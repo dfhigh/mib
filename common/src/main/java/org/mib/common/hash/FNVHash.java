44b6091f0b1bcb8c33c1d9a4d652ff3df441b820
Fei Du <fei.du@autodesk.com>
2019-04-15 12:39:12 +0800

    add hash32 implementation for FNV hash

--- common/src/main/java/org/mib/common/hash/FNVHash.java
+++ common/src/main/java/org/mib/common/hash/FNVHash.java
@@ -7,13 +7,25 @@ public final class FNVHash {
 
     private static final long FNV_64_INIT = 0xcbf29ce484222325L;
     private static final long FNV_64_PRIME = 0x100000001b3L;
+    private static final int FNV_32_INIT = 0x811c9dc5;
+    private static final int FNV_32_PRIME = 0x1000193;
 
     public static long hash64(final String k) {
         long rv = FNV_64_INIT;
         final int len = k.length();
-        for(int i = 0; i < len; i++) {
+        for (int i = 0; i < len; i++) {
             rv ^= k.charAt(i);
             rv *= FNV_64_PRIME;
+        }
+        return rv;
+    }
+
+    public static int hash32(final String k) {
+        int rv = FNV_32_INIT;
+        final int len = k.length();
+        for (int i = 0; i < len; i++) {
+            rv ^= k.charAt(i);
+            rv *= FNV_32_PRIME;
         }
         return rv;
     }
