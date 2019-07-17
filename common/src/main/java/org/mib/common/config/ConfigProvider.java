b981aa818589c924ba46e48d4a5a023bd40cb8db
Fei Du <fei.du@autodesk.com>
2019-07-17 21:13:44 +0800

    1.2.1

--- common/src/main/java/org/mib/common/config/ConfigProvider.java
+++ common/src/main/java/org/mib/common/config/ConfigProvider.java
@@ -37,28 +37,31 @@ public class ConfigProvider {
     }
 
     public static String get(String key) {
-        String raw = CONFIGS.getProperty(key);
-        if (raw != null) {
-            Matcher matcher = WITH_ENV_VALUE.matcher(raw);
-            if (matcher.find()) {
-                String env = matcher.group(1), defaultValueWithColon = matcher.group(2);
-                String envValue = System.getenv(env);
-                if (defaultValueWithColon != null) {
-                    String defaultValue = defaultValueWithColon.substring(1);
-                    return Optional.ofNullable(envValue).orElse(defaultValue);
-                }
-                return envValue;
+        return getValueWithEnv(CONFIGS.getProperty(key));
+    }
+
+    public static String getValueWithEnv(String expression) {
+        if (expression == null) return null;
+        Matcher matcher = WITH_ENV_VALUE.matcher(expression);
+        if (matcher.find()) {
+            String env = matcher.group(1), defaultValueWithColon = matcher.group(2);
+            String envValue = System.getenv(env);
+            if (defaultValueWithColon != null) {
+                String defaultValue = defaultValueWithColon.substring(1);
+                return Optional.ofNullable(envValue).orElse(defaultValue);
             }
+            return envValue;
         }
-        return raw;
+        return expression;
     }
 
     public static String get(String key, String defaultValue) {
         return Optional.ofNullable(get(key)).orElse(defaultValue);
     }
 
-    public static int getInt(String key) {
-        return Integer.parseInt(get(key));
+    public static Integer getInt(String key) {
+        String value = get(key);
+        return value == null ? null : Integer.parseInt(value);
     }
 
     public static int getInt(String key, int defaultValue) {
@@ -66,8 +69,14 @@ public class ConfigProvider {
         return str == null ? defaultValue : Integer.parseInt(str);
     }
 
-    public static long getLong(String key) {
-        return Long.parseLong(get(key));
+    public static Integer getIntWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Integer.parseInt(value);
+    }
+
+    public static Long getLong(String key) {
+        String value = get(key);
+        return value == null ? null : Long.parseLong(value);
     }
 
     public static long getLong(String key, long defaultValue) {
@@ -75,8 +84,14 @@ public class ConfigProvider {
         return str == null ? defaultValue : Long.parseLong(str);
     }
 
-    public static short getShort(String key) {
-        return Short.parseShort(get(key));
+    public static Long getLongWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Long.parseLong(value);
+    }
+
+    public static Short getShort(String key) {
+        String value = get(key);
+        return value == null ? null : Short.parseShort(value);
     }
 
     public static short getShort(String key, short defaultValue) {
@@ -84,8 +99,14 @@ public class ConfigProvider {
         return str == null ? defaultValue : Short.parseShort(str);
     }
 
-    public static double getDouble(String key) {
-        return Double.parseDouble(get(key));
+    public static Short getShortWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Short.parseShort(value);
+    }
+
+    public static Double getDouble(String key) {
+        String value = get(key);
+        return value == null ? null : Double.parseDouble(value);
     }
 
     public static double getDouble(String key, double defaultValue) {
@@ -93,8 +114,14 @@ public class ConfigProvider {
         return str == null ? defaultValue : Double.parseDouble(str);
     }
 
-    public static float getFloat(String key) {
-        return Float.parseFloat(get(key));
+    public static Double getDoubleWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Double.parseDouble(value);
+    }
+
+    public static Float getFloat(String key) {
+        String value = get(key);
+        return value == null ? null : Float.parseFloat(value);
     }
 
     public static float getFloat(String key, float defaultValue) {
@@ -102,8 +129,14 @@ public class ConfigProvider {
         return str == null ? defaultValue : Float.parseFloat(str);
     }
 
-    public static char getChar(String key) {
-        return get(key).charAt(0);
+    public static Float getFloatWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Float.parseFloat(value);
+    }
+
+    public static Character getChar(String key) {
+        String value = get(key);
+        return value == null ? null : value.charAt(0);
     }
 
     public static char getChar(String key, char defaultValue) {
@@ -111,12 +144,23 @@ public class ConfigProvider {
         return str == null || str.isEmpty() ? defaultValue : str.charAt(0);
     }
 
-    public static boolean getBoolean(String key) {
-        return Boolean.parseBoolean(get(key));
+    public static Character getCharWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : value.charAt(0);
+    }
+
+    public static Boolean getBoolean(String key) {
+        String value = get(key);
+        return value == null ? null : Boolean.parseBoolean(value);
     }
 
     public static boolean getBoolean(String key, boolean defaultValue) {
         String str = get(key);
         return str == null ? defaultValue : Boolean.parseBoolean(str);
+    }
+
+    public static Boolean getBooleanWithEnv(String expression) {
+        String value = getValueWithEnv(expression);
+        return value == null ? null : Boolean.parseBoolean(value);
     }
 }
